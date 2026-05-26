7aa257fe140557ec4d348332ee401c51d8ac6cbb
Stanislav <staskobetc@gmail.com>
2026-05-26 19:30:33 +0300

    add templates: index.html, task_detail.html, worker_detail.html, worker_list.html. change views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Task, Worker
 
-# Create your views here.
+
+def index(request):
+    context = {
+        "task_count": Task.objects.count(),
+        "worker_count": Worker.objects.count(),
+        "completed_tasks": Task.objects.filter(is_completed=True).count(),
+    }
+    return render(request, "catalog/index.html", context)
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    paginate_by = 10
+
+
+class TaskDetailView(generic.DetailView):
+    model = Task
+
+
+class WorkerListView(generic.ListView):
+    model = Worker
+    paginate_by = 10
+
+
+class WorkerDetailView(generic.DetailView):
+    model = Worker
