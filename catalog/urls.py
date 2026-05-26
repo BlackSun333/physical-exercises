3d576bdfb57583e257341a33e47e756fb68265fb
Stanislav <staskobetc@gmail.com>
2026-05-26 20:38:03 +0300

    catalog/urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,17 +4,22 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
 
+    # Auth
+    path("login/", views.WorkerLoginView.as_view(), name="login"),
+    path("logout/", views.WorkerLogoutView.as_view(), name="logout"),
+    path("register/", views.WorkerCreateView.as_view(), name="register"),
+
     # Tasks
     path("tasks/", views.TaskListView.as_view(), name="task-list"),
-    path("tasks/<int:pk>/", views.TaskDetailView.as_view(), name="task-detail"),
     path("tasks/create/", views.TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/", views.TaskDetailView.as_view(), name="task-detail"),
     path("tasks/<int:pk>/update/", views.TaskUpdateView.as_view(), name="task-update"),
     path("tasks/<int:pk>/delete/", views.TaskDeleteView.as_view(), name="task-delete"),
 
     # Workers
     path("workers/", views.WorkerListView.as_view(), name="worker-list"),
-    path("workers/<int:pk>/", views.WorkerDetailView.as_view(), name="worker-detail"),
     path("workers/create/", views.WorkerCreateView.as_view(), name="worker-create"),
+    path("workers/<int:pk>/", views.WorkerDetailView.as_view(), name="worker-detail"),
     path("workers/<int:pk>/update/", views.WorkerUpdateView.as_view(), name="worker-update"),
     path("workers/<int:pk>/delete/", views.WorkerDeleteView.as_view(), name="worker-delete"),
 ]
